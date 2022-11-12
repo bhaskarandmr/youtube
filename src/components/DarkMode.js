f21da75fbf499936760a707405ef0f6fb62c9be1
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-11-12 18:29:43 +0530

    Update DarkMode.js

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -3,7 +3,10 @@ import { Grid, Typography, Switch } from '@material-ui/core'
 
 const DarkMode = ({onChange}) => {
     return (
-        
+        <Grid alignItems="center" justify="center" container spacing={2}>
+            <Typography></Typography>
+            <Switch onChange={onChange} color="primary"></Switch>
+        </Grid>
     )
 }
 
