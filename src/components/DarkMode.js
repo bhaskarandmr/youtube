9f59017213ae06090bc6e915acd5aec0c917ccdc
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-11-12 18:30:48 +0530

    Update DarkMode.js

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -5,7 +5,6 @@ const DarkMode = ({onChange}) => {
     return (
         <Grid alignItems="center" justify="center" container spacing={2}>
             <Typography></Typography>
-            <Switch onChange={onChange} color="primary"></Switch>
         </Grid>
     )
 }
